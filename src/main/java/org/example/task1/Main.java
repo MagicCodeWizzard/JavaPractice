0c62f3f9474286ebecc3f8092840f4f4d4835fbc
Student <andrii.kuliak@gmail.com>
2024-11-26 12:02:36 +0200

    Corrected task 1

--- src/main/java/org/example/task1/Main.java
+++ src/main/java/org/example/task1/Main.java
@@ -1,7 +1,9 @@
-package org.example;
+package org.example.task1;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println("Hello again!");
     }
 }
